b89d66d11eda1b1e3d3c833d3ae3afc5ed9c267c
Guillaume Delecourt <g.delecour@apefe.org>
2013-03-25 12:22:33 +0100

    ajout du stockage du "contact" dans un fichier csv

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -33,73 +33,62 @@ class Home_Controller extends Base_Controller {
 
 	public function get_index()
 	{
-		/*for ($i=1; $i<=10; $i++)                                  
-		{
-		$experience = new Experience();
-		$experience->nom="entreprise $i";
-		$experience->adresse="adresse $i";
-		$experience->site="site $i";
-		$experience->description="description $i";
-		$experience->debut=date("m.d.y");
-		$experience->fin=date("m.d.y");
-		$experience->save();
-
-	}*/
-	$experiences=Experience::all();
-	return View::make('home.index')->with('experiences',$experiences);
-}
-
-public function get_contact()
-{
-	return View::make('home.contact');
-}
-
-public function post_contact()
-{
-	$nom = Input::get('nom');
-	$prenom = Input::get('prenom');
-	$email = Input::get('email');
-	$message = Input::get('message');
-
-		$contact = new Contact();
-		$contact->nom=$nom;
-		$contact->prenom=$prenom;
-		$contact->email=$email;
-		$contact->contenu=$message;
-		$contact->save();
-
-
-	$mailer = IoC::resolve('mailer');
-	$message = Swift_Message::newInstance("Message de $nom ($email)")
-	->setFrom(array('webmaster@agnus.me'=>'site cv'))
-	->setTo(array('ariane@agnus.me'=>'Ariane'))
-	->setBody($message,'text/html');
-		// Send the email
-
-	 	//echo $nom $email;
-	Input::flash();
-
-	if (empty($nom) OR empty($prenom) OR empty($email) OR empty($message))
-	{
-		Session::flash('status_error', 'Une erreur est survenue. Veillez à remplir tous les champs.');
+		$experiences=Experience::all();
+		return View::make('home.index')->with('experiences',$experiences);
 
 	}
 
-	else
+	public function get_contact()
 	{
-		
-		if($mailer->send($message))
+		return View::make('home.contact');
+	}
+
+	public function post_contact()
+	{
+		$nom = Input::get('nom');
+		$prenom = Input::get('prenom');
+		$email = Input::get('email');
+		$message = Input::get('message');
+
+		// on prepare le mail
+		$mailer = IoC::resolve('mailer');
+		$message = Swift_Message::newInstance("Message de $nom ($email)")
+		->setFrom(array('webmaster@agnus.me'=>'site cv'))
+		->setTo(array('ariane@agnus.me'=>'Ariane'))
+		->setBody($message,'text/html');
+
+		Input::flash();
+		if (empty($nom) OR empty($prenom) OR empty($email) OR empty($message))
 		{
-			Session::flash('status_success', 'Message envoyé');
+			Session::flash('status_error', 'Une erreur est survenue. Veillez à remplir tous les champs.');
+
 		}
 		else
 		{
-			Session::flash('status_error', "Une erreur d'envoi a eu lieu");
+			
+			//  on stocke dans la DB
+			$contact = new Contact();
+			$contact->nom=$nom;
+			$contact->prenom=$prenom;
+			$contact->email=$email;
+			$contact->contenu=$message;
+			$contact->save();
+
+			// on stocke dans le fichier txt (csv)
+			File::append('storage/database/contacts.csv', "$nom,$prenom,$email,$message\n");
+
+			if($mailer->send($message))
+			{
+				Session::flash('status_success', 'Message envoyé');
+			}
+			else
+			{
+				Session::flash('status_error', "Une erreur d'envoi a eu lieu");
+			}
+
 		}
-		
+		return  Redirect::to('home/contact');
 	}
-	return  Redirect::to('home/contact');
-}
 
 }
 
